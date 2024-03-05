2b048b7beb7535165cdca28682bb2f63dfebe572
ignatius21 <ignaciojosealberto@hotmail.com>
2024-03-05 20:54:34 -0300

    commit inicial

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import ButtonSvg from '../assets/svg/ButtonSvg';
+
+const Button = ({className, href, onClick, children, px, white}) => {
+    const clasess = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`;
+    const spanClasses = 'relative z-10';
+  const renderButton = () =>(
+    <button className={clasess}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+    </button>
+  )
+
+  const renderLink = () => (
+    <a href={href} className={clasess}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton(); 
+}
+
+export default Button

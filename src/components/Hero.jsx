a461ad9f2d5a7a7f0f3bbb8a4077a75033339821
ignatius21 <ignaciojosealberto@hotmail.com>
2024-03-07 20:55:38 -0300

    Añadidos componentes Generating y Notification a Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,8 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
@@ -54,6 +56,9 @@ const Hero = () => {
                   alt="AI"
                 />
                 <ScrollParallax isAbsolutelyPositioned>
+                  <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
                       <li className="p-5" key={index}>
@@ -61,6 +66,12 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code Notification"
+                  />
                 </ScrollParallax>
               </div>
             </div>

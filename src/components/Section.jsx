4c3eddb861a00e2b3a5333c465aa4386a53bf927
ignatius21 <ignaciojosealberto@hotmail.com>
2024-03-07 20:10:15 -0300

    Agregadas dependencias de react-just-parallax y corregido import en Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,4 @@
-import SectionSvg from '../assets/svg/SectionSvg'
+import SectionSvg from "../assets/svg/SectionSvg";
 
 const Section = ({
   className,
@@ -11,16 +11,19 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`relative ${
+      className={`
+      relative 
+      ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
     >
       {children}
+
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
       <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
+
       {crosses && (
         <>
           <div
@@ -35,4 +38,4 @@ const Section = ({
   );
 };
 
-export default Section
+export default Section;

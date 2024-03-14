ab272367c981de789876e685ec60f584a3bc317d
ignatius21 <ignaciojosealberto@hotmail.com>
2024-03-13 21:04:51 -0300

    Añadir importación y uso de CompanyLogos en Hero.jsx

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,7 +1,17 @@
+import { companyLogos } from "../constants"
 
-const CompanyLogos = () => {
+const CompanyLogos = ({className}) => {
   return (
-    <div>CompanyLogos</div>
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">Helping people create beatiful content at</h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li key={index} className="flex items-center justify-center flex-1 h-[8.5rem]">
+            <img src={logo} width={134} height={28} alt={logo} />
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 

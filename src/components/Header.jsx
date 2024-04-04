5e93fc6b557683e75f757742369ac76c02607df3
ignatius21 <ignaciojosealberto@hotmail.com>
2024-04-04 13:28:36 -0300

    Actualizar texto del botón "New Account" a "Nueva Cuenta" en el componente Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
                     <HambugerMenu/>
             </nav>
             <a href='#signup' className='button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block'>
-                New Account
+                Nueva Cuenta
             </a>
             <Button className='hidden lg:flex' href='#login'>
                 Sign In
